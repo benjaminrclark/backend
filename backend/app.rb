d846e86bbee79eb29e4b5c43d8614e34fd9987f8
Ben Clark <ben.clark02@bbc.co.uk>
2017-07-10 11:38:04 +0000

    configurable status

--- backend/app.rb
+++ backend/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 set :bind, '0.0.0.0'
 set :port, ENV.fetch('PORT','4567')
@@ -6,5 +7,7 @@ set :logging, true
  
 get '/' do
   content_type 'application/json'
+  config = JSON.parse(File.read(ENV.fetch("CONFIG","/etc/app.conf")))
+  status config['status']
   File.read(ENV.fetch("CONFIG",'/etc/app.conf')) 
 end
